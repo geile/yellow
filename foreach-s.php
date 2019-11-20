334f9d25f5723f550aae8d88c0b61c4f37c5be1f
unknown <tim.vnetzrlernede.bfo-vs.ch>
2019-11-20 14:55:55 +0100

    Add foreach loop

---
+++ foreach-s.php
@@ -0,0 +1,18 @@
+
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+	<?php $products = ['Apel','Banana','Clementine'];
+		  $gap ="  ";
+		 foreach($products as $product){
+			echo $product ;
+			echo $gap;
+		}	
+		?>
+		
+    </body>
+	
+</html>
