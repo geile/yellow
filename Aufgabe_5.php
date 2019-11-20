d7041922d005424cf13358d016392937c542dee2
Feger Luc <luc.feger@lernende.bfo-vs.ch>
2019-11-20 15:05:40 +0100

    Excersies 5

---
+++ Aufgabe_5.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+<?php
+    $letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+$break = 3;
+foreach ($letters as $index => $letter) {
+    if ($index % $break == 0) {
+        echo "<br>";
+    }
+    echo $letter . " ";
+}
+
+echo "<br>";
+echo "<br>";
