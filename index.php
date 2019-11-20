3d2ce6d1f537b2f69585ee31693e8d8f3373ccfd
Feger Luc <luc.feger@lernende.bfo-vs.ch>
2019-11-20 15:05:10 +0100

    Excersies 5

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ echo "hallo";
     <a href="Aufgabe_2.php">Aufgabe 2</a>
     <br>
     <a href="Aufgabe_3.php">Aufgabe 3</a>
+    <br>
+    <a href="Aufgabe_5.php">Aufgabe 5</a>
 </p>
 </body>
 </html>
